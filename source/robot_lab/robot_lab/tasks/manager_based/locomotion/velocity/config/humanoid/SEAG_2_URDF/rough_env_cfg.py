5fa76420b0ab6040abbf6dd234b53d2f1b491723
Weijie Wang <duangsir518@163.com>
2025-12-24 13:32:59 +0800

    feet_horizontal_simple joint_acc_l2 joint_torques_l2
    feet_horizontal_simple.weight = 1.0
    self.rewards.joint_acc_l2.weight = -2.0e-7
    self.rewards.joint_torques_l2.weight = -4.0e-4

--- source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/humanoid/SEAG_2_URDF/rough_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/humanoid/SEAG_2_URDF/rough_env_cfg.py
@@ -131,7 +131,7 @@ class SEAG_2_URDFRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.feet_height_body.params["target_height"] = -0.32
         self.rewards.feet_height_body.params["asset_cfg"].body_names = [self.foot_link_name]
         self.rewards.upward.weight = 1.0
-        self.rewards.feet_horizontal_simple.weight = 0.5
+        self.rewards.feet_horizontal_simple.weight = 1.0
         self.rewards.feet_horizontal_simple.params["asset_cfg"].body_names = [self.foot_link_name]
 
         # If the weight of rewards is 0, set rewards to None
