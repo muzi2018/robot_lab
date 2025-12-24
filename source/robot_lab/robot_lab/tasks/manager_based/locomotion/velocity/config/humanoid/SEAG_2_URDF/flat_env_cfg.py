8962b04f267e1e0a1ec2e7faae20e26861e5f585
Weijie Wang <duangsir518@163.com>
2025-12-24 13:42:37 +0800

    joint_torques_l2
    self.rewards.joint_torques_l2.params["asset_cfg"].joint_names = [".*_hip_roll.*", ".*_hip_yaw.*",".*_ankle_roll_Joint"]

--- source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/humanoid/SEAG_2_URDF/flat_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/humanoid/SEAG_2_URDF/flat_env_cfg.py
@@ -34,7 +34,7 @@ class SEAG_2_URDFFlatEnvCfg(SEAG_2_URDFRoughEnvCfg):
         self.rewards.feet_air_time.weight = 0.75
         self.rewards.feet_air_time.params["threshold"] = 1.5
         self.rewards.joint_torques_l2.weight = -4.0e-4 # -2.0e-4
-        self.rewards.joint_torques_l2.params["asset_cfg"].joint_names = [".*_hip_.*", ".*_knee_pitch_Joint"]
+        self.rewards.joint_torques_l2.params["asset_cfg"].joint_names = [".*_hip_roll.*", ".*_hip_yaw.*",".*_ankle_roll_Joint"]
 
         # If the weight of rewards is 0, set rewards to None
         if self.__class__.__name__ == "SEAG_2_URDFFlatEnvCfg":
