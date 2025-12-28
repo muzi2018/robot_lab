aec3e2c536accf54ee37dc1033f42fd4826410b8
Weijie Wang <duangsir518@163.com>
2025-12-28 19:36:53 +0800

    decimation
    dt = 0.002, decimation = 10

--- source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -707,10 +707,10 @@ class LocomotionVelocityRoughEnvCfg(ManagerBasedRLEnvCfg):
     def __post_init__(self):
         """Post initialization."""
         # general settings
-        self.decimation = 4
+        self.decimation = 10
         self.episode_length_s = 20.0
         # simulation settings
-        self.sim.dt = 0.005
+        self.sim.dt = 0.002
         self.sim.render_interval = self.decimation
         self.sim.physics_material = self.scene.terrain.physics_material
         self.sim.physx.gpu_max_rigid_patch_count = 10 * 2**15
